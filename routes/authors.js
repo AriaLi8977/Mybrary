b1afc6b35dd8446d50b95d69e45cb3914ccd80a3
AriaXYLi <ariali8977@gmail.com>
2025-04-25 15:58:11 +0800

    change author routes

--- routes/authors.js
+++ routes/authors.js
@@ -5,14 +5,20 @@ const Author = require('../models/author')
 
 //All authors router
 router.get('/', async (req,res)=>{
+    let searchOptions = {}
+    //get request is sent through query string ...?name=jo
+    if(req.query.name !== null && req.query.name !== ''){ //if there's a query sent to the server
+        searchOptions.name = new RegExp(req.query.name, 'i') //case insensitive
+    } 
     try{
-        const authors = await Author.find({}) //find objects with no condition
-        res.render('authors/index', {authors: authors}) //passing var on the line before 
+        const authors = await Author.find(searchOptions) //find objects with no searchOptions
+        res.render('authors/index', 
+            {authors: authors, //passing var on the line before 
+            searchOptions:req.query}) //send back the query to user
     }catch{
         res.redirect('/')
+
     }
-
-
 })
 
 //new authors router
@@ -23,7 +29,7 @@ router.get('/new',(req,res)=>{
 //create author route
 router.post('/', async (req,res)=>{
     const author = new Author({
-        name: req.body.name
+        name: req.body.name //post request is sent through body
     })
     try{
         const newAuthor = await author.save()//wait for this to complete
